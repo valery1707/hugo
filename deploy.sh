12a6aca1b57ae7d75deb598b28ed7507593fe3b8
Sadique Ali <sadiqalikm@gmail.com>
2015-05-21 22:08:53 -0700

    Blog updated at Thu May 21 22:08:53 PDT 2015

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Deploy blog to GitHub pages
-
-hugo
-git add -f public/
-git commit -m "Blog updated at $(date)"
-declare remote=$(git remote -v | grep fetch | awk '{print $2}')
-git subtree push --prefix=public $remote gh-pages
