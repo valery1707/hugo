2e648cbeafad0efed65169d12bd78cd9952a34dc
Sadique Ali <sadiqalikm@gmail.com>
2015-05-21 22:08:47 -0700

    Force add public/ when deploying.

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # Deploy blog to GitHub pages
 
 hugo
-git add public/
+git add -f public/
 git commit -m "Blog updated at $(date)"
 declare remote=$(git remote -v | grep fetch | awk '{print $2}')
 git subtree push --prefix=public $remote gh-pages
